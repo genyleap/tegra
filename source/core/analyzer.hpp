b2996feca2566b5d2c93f33ed2930c04134dc558
Kambiz Asadzadeh <kambiz.ceo@gmail.com>
2022-08-31 13:14:57 +0430

    Update Analyzer.

--- source/core/analyzer.hpp
+++ source/core/analyzer.hpp
@@ -15,11 +15,36 @@
 
 #include "common.hpp"
 #include "core.hpp"
-#include "core/config.hpp"
+#include "translator/language.hpp"
+#include "translator/translator.hpp"
 
 TEGRA_USING_NAMESPACE Tegra::Types;
 
 TEGRA_NAMESPACE_BEGIN(Tegra::System)
+
+#define MODULES "modules"
+#define PLUGINS "plugins"
+#define CHECKSUMS_FILE ".txt"
+
+enum class CheckType
+{
+    MD5, SHA1, SHA256, SHA512
+};
+
+struct ChecksumData final
+{
+    std::string message     {};
+    std::string algorithm   {};
+    CheckType checkType     {};
+};
+
+struct ModuleAnalyzerData final
+{
+    bool isExist        {false};
+    bool isSupported    {false};
+    bool isOfficial     {false};
+    std::string message {__tegra_null_str};
+};
 
 class ModuleAnalyzer
 {
@@ -33,13 +58,31 @@ public:
      * \returns true if the module exists.
      */
     __tegra_no_discard bool isExist(const std::string& moduleName) __tegra_noexcept;
+
+    /*!
+     * \brief isOfficial function will checks module creator.
+     * \param moduleName as module string name.
+     * \returns true if the module created by official owner or developers.
+     */
+    __tegra_no_discard bool isOfficial(const std::string& moduleName) __tegra_noexcept;
+
+    /*!
+     * \brief isSupported function will ensures that the desired file is supported by the system.
+     * \param moduleName as module string name.
+     * \returns true if the module supported by system.
+     */
+    __tegra_no_discard bool isSupported(const std::string& moduleName) __tegra_noexcept;
+
+    /*!
+     * \brief result function will gets message of status.
+     * \returns as string message.
+     */
     __tegra_no_discard OptionalString result() __tegra_noexcept;
 
 private:
     ApplicationData* appDataPtr{};
     Scope<Multilangual::Language> languagePtr{};
-    bool m_isExist{false};
-    std::string m_message{};
+    ModuleAnalyzerData m_moduleAnalyzerData;
 
 };
 
@@ -54,14 +97,53 @@ public:
      * \param pluginName as plugin string name.
      * \returns true if the plugin exists.
      */
+    /*!
+     * \brief isExist function checks plugin exist or not.
+     * \param pluginName as plugin string name.
+     * \returns true if the plugin exists.
+     */
     __tegra_no_discard bool isExist(const std::string& pluginName) __tegra_noexcept;
+
+    /*!
+     * \brief isOfficial function will checks plugin creator.
+     * \param pluginName as plugin string name.
+     * \returns true if the plugin created by official owner or developers.
+     */
+    __tegra_no_discard bool isOfficial(const std::string& pluginName) __tegra_noexcept;
+
+    /*!
+     * \brief isSupported function will ensures that the desired file is supported by the system.
+     * \param pluginName as plugin string name.
+     * \returns true if the plugin supported by system.
+     */
+    __tegra_no_discard bool isSupported(const std::string& pluginName) __tegra_noexcept;
+
+    /*!
+     * \brief result function will gets message of status.
+     * \returns as string message.
+     */
     __tegra_no_discard OptionalString result() __tegra_noexcept;
 
 private:
     ApplicationData* appDataPtr{};
     Scope<Multilangual::Language> languagePtr{};
-    bool m_isExist{false};
-    std::string m_message{};
+    ModuleAnalyzerData m_moduleAnalyzerData;
+};
+
+class Checksum
+{
+public:
+    Checksum(const CheckType& checkType);
+    ~Checksum();
+
+    __tegra_no_discard bool checkFile(const std::string& path) __tegra_noexcept;
+    __tegra_no_discard bool generate(const std::string& file) __tegra_noexcept;
+    __tegra_no_discard std::string getResult() __tegra_noexcept;
+
+    void setResult(const std::string& r) __tegra_noexcept;
+
+private:
+    ChecksumData m_checkSumData;
 
 };
 
